c2c898c27fa762d6687ca88bd59d3587071f4490
Vicky <ying.vicky.cao@outlook.com>
2019-10-08 20:28:55 +0800

    Update Person.java

--- app/src/main/java/com/github/yingvickycao/enablecodecoverage/Person.java
+++ app/src/main/java/com/github/yingvickycao/enablecodecoverage/Person.java
@@ -4,6 +4,7 @@ package com.github.yingvickycao.enablecodecoverage;
 
 public class Person {
     public String name;
+    private int n = 3;
 
     public Person(String name) {
         this.name = name;
@@ -15,11 +16,13 @@ public class Person {
         return name;
     }
 
-    public void handleStu(Stu stu){
-        if (null == stu){
+    public void handleStu(Stu stu) {
+        if (null == stu) {
             System.out.println(stu.toString());
         }
 
+        n = 5;
+        n++;
         /**
          * (Constanst.ONE == 1){
          *  System.out.println(Constanst.ONE);
