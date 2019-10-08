2528e1d4cb595c0b5495b8e8a873d943fd7ea399
Vicky <ying.vicky.cao@outlook.com>
2019-10-08 14:24:50 +0800

    remove no use code

--- app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
+++ app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class SecondActivityTest {
-    
+
     @get:Rule
     var activityRule = ActivityTestRule(SecondActivity::class.java)
 
