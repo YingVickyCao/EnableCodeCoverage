de9d024e1a9af7339dfc55e468f83e0916880a75
Vicky <ying.vicky.cao@outlook.com>
2019-10-08 14:23:52 +0800

    Update SecondActivityTest.kt

--- app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
+++ app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
@@ -13,15 +13,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class SecondActivityTest {
-
-//    @Test
-//    fun useAppContext() {
-//        // Context of the app under test.
-//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-//        Assert.assertEquals("com.github.yingvickycao.enablecodecoverage", appContext.packageName)
-//    }
-//
-
+    
     @get:Rule
     var activityRule = ActivityTestRule(SecondActivity::class.java)
 
