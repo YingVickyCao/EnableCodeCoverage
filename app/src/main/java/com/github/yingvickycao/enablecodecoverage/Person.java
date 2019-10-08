3c679a9f283f3a4a487a501f4ef3a6c0101a1660
Vicky <ying.vicky.cao@outlook.com>
2019-10-08 18:05:57 +0800

    Update Person.java

--- app/src/main/java/com/github/yingvickycao/enablecodecoverage/Person.java
+++ app/src/main/java/com/github/yingvickycao/enablecodecoverage/Person.java
@@ -1,13 +1,15 @@
 package com.github.yingvickycao.enablecodecoverage;
 
+
 public class Person {
     private String name;
 
     public Person(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
+//        Log.d("Person", "getName: "+ name);
         return name;
     }
 }
