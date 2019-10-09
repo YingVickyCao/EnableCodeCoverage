9b688d98f6b746fb9e76262e48d080951857dd18
Vicky <ying.vicky.cao@outlook.com>
2019-10-09 11:41:40 +0800

    gradle:4.5 -> change libs version

--- app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
+++ app/src/androidTest/java/com/github/yingvickycao/enablecodecoverage/SecondActivityTest.kt
@@ -1,17 +1,15 @@
 package com.github.yingvickycao.enablecodecoverage
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
-import org.junit.Assert
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
 import org.junit.Test
 
 import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
+
 class SecondActivityTest {
 
     @get:Rule
--- app/src/main/java/com/github/yingvickycao/enablecodecoverage/SecondActivity.kt
+++ app/src/main/java/com/github/yingvickycao/enablecodecoverage/SecondActivity.kt
@@ -1,7 +1,7 @@
 package com.github.yingvickycao.enablecodecoverage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
