3d334206c08708388bdfaa9314cffad69a517d10
YingVickyCao <ying.vicky.cao@outlook.com>
2021-08-11 14:19:37 +0800

    use androidx

--- app/src/main/java/com/github/yingvickycao/enablecodecoverage/SecondActivity.kt
+++ app/src/main/java/com/github/yingvickycao/enablecodecoverage/SecondActivity.kt
@@ -1,8 +1,8 @@
 package com.github.yingvickycao.enablecodecoverage
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.github.yingvickycao.androidlib.B2
 import com.github.yingvickycao.javalib.C2
 
